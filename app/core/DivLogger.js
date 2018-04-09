74c735ce3158af3291094943df949443aa04a7e5
richard-diamond <richard.diamond@hmcts.net>
2018-04-09 14:54:57 +0100

    DIV-1931: simplified implementation

--- app/core/DivLogger.js
+++ app/core/DivLogger.js
@@ -1,57 +1,25 @@
+const IdamService = require('app/services/idam');
 const NodejsLogger = require('@hmcts/nodejs-logging');
-const clientId = require('app/services/idam').clientId();
 
 class DivLogger {
-  // **
-  // * Public functions
-  // **
-
-  constructor(filename) {
-    this.nodejsLogger = NodejsLogger.getLogger(filename);
-  }
-
-  info(message) {
-    this.nodejsLogger.info(`${this._getMessagePrefix()} ${message}`);
-  }
-
-  warn(message) {
-    this.nodejsLogger.warn(`${this._getMessagePrefix()} ${message}`);
-  }
-
-  error(message) {
-    this.nodejsLogger.error(`${this._getMessagePrefix()} ${message}`);
-  }
-
-  fatal(message) {
-    this.nodejsLogger.trace(`${this._getMessagePrefix()} ${message}`);
-  }
-
-  trace(message) {
-    this.nodejsLogger.trace(`${this._getMessagePrefix()} ${message}`);
-  }
-
-  debug(message) {
-    this.nodejsLogger.debug(`${this._getMessagePrefix()} ${message}`);
-  }
-
   // **
   // * Public static functions
   // **
 
   static getLogger(name) {
-    return new DivLogger(name);
-  }
+    const logger = NodejsLogger.getLogger(name);
 
-  static config(config) {
-    NodejsLogger.config(config);
-  }
+    logger._oldWrap = logger._wrap;
+    logger._wrap = function _wrap(message) {
+      const logEntry = this._oldWrap(message);
 
-  // **
-  // * Private functions
-  // **
+      logEntry.uid = IdamService.clientId() || 'unknown';
+      logEntry.message = `${logEntry.uid}: ${logEntry.message}`;
 
-  _getMessagePrefix() {
-    return `(uid: ${clientId})`;
+      return logEntry;
+    };
+
+    return logger;
   }
 }
 
--- app/core/DivLogger.test.js
+++ app/core/DivLogger.test.js
@@ -1,15 +1,15 @@
 const { expect } = require('test/util/chai');
+const clientId = require('app/services/idam').clientId();
 
 const modulePath = 'app/core/DivLogger';
 const divLogger = require(modulePath).getLogger(__filename);
 
 describe(modulePath, () => {
-  const expectedPrefix = '(uid: ';
   let loggedMessage = '';
 
   // the following really only needs to be performed once, but as beforeAll is not available, beforeEach has been used
   beforeEach(() => {
-    const outputter = divLogger.nodejsLogger;
+    const outputter = divLogger.logger;
     if (!outputter) return;
 
     function intercept(method) {
@@ -37,49 +37,49 @@ describe(modulePath, () => {
 
   it('adds id to info logging request', () => {
     const message = 'this is an info message';
+    const expected = `${clientId}: ${message}`;
 
     divLogger.info(message);
-    expect(loggedMessage.startsWith(expectedPrefix)).to.eql(true);
-    expect(loggedMessage.endsWith(message)).to.eql(true);
+    expect(loggedMessage.endsWith(expected)).to.eql(true);
   });
 
   it('adds id to warning logging request', () => {
     const message = 'this is a warning message';
+    const expected = `${clientId}: ${message}`;
 
     divLogger.warn(message);
-    expect(loggedMessage.startsWith(expectedPrefix)).to.eql(true);
-    expect(loggedMessage.endsWith(message)).to.eql(true);
+    expect(loggedMessage.endsWith(expected)).to.eql(true);
   });
 
   it('adds id to error logging request', () => {
     const message = 'this is an error message';
+    const expected = `${clientId}: ${message}`;
 
     divLogger.error(message);
-    expect(loggedMessage.startsWith(expectedPrefix)).to.eql(true);
-    expect(loggedMessage.endsWith(message)).to.eql(true);
+    expect(loggedMessage.endsWith(expected)).to.eql(true);
   });
 
   it('adds id to fatal logging request', () => {
     const message = 'this is a fatal message';
+    const expected = `${clientId}: ${message}`;
 
     divLogger.fatal(message);
-    expect(loggedMessage.startsWith(expectedPrefix)).to.eql(true);
-    expect(loggedMessage.endsWith(message)).to.eql(true);
+    expect(loggedMessage.endsWith(expected)).to.eql(true);
   });
 
   it('adds id to debug logging request', () => {
     const message = 'this is a debug message';
+    const expected = `${clientId}: ${message}`;
 
     divLogger.debug(message);
-    expect(loggedMessage.startsWith(expectedPrefix)).to.eql(true);
-    expect(loggedMessage.endsWith(message)).to.eql(true);
+    expect(loggedMessage.endsWith(expected)).to.eql(true);
   });
 
   it('adds id to trace logging request', () => {
     const message = 'this is a trace message';
+    const expected = `${clientId}: ${message}`;
 
     divLogger.trace(message);
-    expect(loggedMessage.startsWith(expectedPrefix)).to.eql(true);
-    expect(loggedMessage.endsWith(message)).to.eql(true);
+    expect(loggedMessage.endsWith(expected)).to.eql(true);
   });
 });
